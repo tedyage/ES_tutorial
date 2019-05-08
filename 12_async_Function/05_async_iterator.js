26392f5fb15d01fa67a6f954eed855210a5238cc
tedyage <tedyage@sina.com>
2019-05-08 22:21:55 +0800

    complete 05_async_iterator.js

--- 12_async_Function/05_async_iterator.js
+++ 12_async_Function/05_async_iterator.js
@@ -2,6 +2,7 @@
 异步遍历器
 */
 const fetch = require("node-fetch");
+const fs = require('fs');
 //异步遍历器，与同步遍历器相比，最大的不同在于
 //同步遍历器每次调用next()，返回的是遍历器指针对象
 //异步遍历器每次调用next()，返回的是Promise对象，等到Promise对象resolve了，才会返回遍历器指针对象
@@ -24,47 +25,34 @@ const fetch = require("node-fetch");
     //这个是同步遍历器
 
 
-    //我们先创建一个异步的Generator方法
-    //语法上是async函数与Generator函数的结合
-    //该方法返回的就是异步遍历器
-    async function* asyncGen(arr){
-        for(let i = 0 ; i < arr.length ; i++){
-            yield arr[i];
-        }
-    }
-
-    let asyncIterator = asyncGen(arr);
-    let promise = asyncIterator.next();    
-    console.log(promise);                  //{pending}
-    promise.then(data=>console.log(data)); //{value:1,done:false}
-    promise = asyncIterator.next()
-    promise.then(data=>console.log(data)); //{value:2,done:false}
-    promise = asyncIterator.next();
-    promise.then(data=>console.log(data)); //{value:3,done:false}
-    promise = asyncIterator.next();
-    promise.then(data=>console.log(data)); //{value:undefined,done:true}
-
-    //异步遍历器返回的是promise对象，
-    //等到promise对象resolve之后，才会返回遍历器指针对象
-
-    //异步遍历器适合在for...of循环中编写异步操作。
-    //这些异步操作在异步遍历器中是并发执行的。
+    
 }
 
 {
     //异步遍历for await... of
+    //对于拥有Symbol.asynciterator遍历器的对象，我们可以使用for await...of来遍历他。
+    //如果使用for...of来遍历它的话，每次遍历得到的都是Promise对象。
+    //等到Promise对象resolved之后，才会得到遍历器指针对象内部的值。
 
-    //如果我们用for...of来做异步便利，写法是这样的。
-    let urls = ["https://api.github.com/users/github","https://api.github.com/users/github","https://api.github.com/users/github"];
-    async function* asyncFetchUrls(urls){
-        for(let i = 0 ; i<urls.length;i++){
-            yield fetch(urls[i]);
+    //异步遍历经常用于流式读取文件
+    //普通的读取文件方法是这样的。
+    let file = "./12_async_Function/assets/a.txt";
+    let readStream = fs.createReadStream(file,{encoding:'utf-8'});
+
+    readStream.on('data',function(chunk){
+        console.log(`...${chunk}`);
+    });
+    readStream.on('end',function(){
+        console.log('...end...');
+    });
+
+    //如果用异步遍历器的读取方法，代码就简洁很多
+    (async()=>{
+        let readStream2 = fs.createReadStream(file,{encoding:'utf-8'});
+        for await (let chunk of readStream2){
+            console.log(`...${chunk}`);
         }
-    }
-
-    let asyncFetch = asyncFetchUrls(urls);
-    console.log(asyncFetch.next());
-    // for(let promise of asyncFetch){
-    //     promise.then(data=>console.log(data));
-    // }
+        console.log('...end...');
+    })();
+    
 }
