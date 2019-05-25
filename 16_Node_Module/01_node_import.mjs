e7a85cb6101af8ef64649d30e0e658a2bacf8122
tedyage <tedyage@sina.com>
2019-05-25 22:22:16 +0800

    add 01_node_import.mjs

--- 16_Node_Module/01_node_import.mjs
+++ 16_Node_Module/01_node_import.mjs
@@ -10,3 +10,7 @@ Node有独立的CommonJS模式来引入模块，与ES6是不兼容的，
 //由于该功能目前仍处于试验阶段，所以要用--experimental-module参数才能开启该功能。
 import app from './assets/my_app.mjs';
 console.log(app);    //{name:'tianzhiqiang',birth:2019-05-25T00:00:00.000Z}
+
+//2.通过安装babel-cli babel-preset-env来让NodeJs兼容ES6的模块语法，
+//详情请看02_babel_node.js
+
